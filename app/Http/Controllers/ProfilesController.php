dbfa9123e1c8ceedefc79d2aa100d6f5306d45cc
Lawrence Chen <50194219+lawrencechen0921@users.noreply.github.com>
2020-07-26 01:05:30 +0800

    updated

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index($user)
+    {
+
+        $user = User::findOrFail($user);
+        return view('home', [
+            'user' => $user,
+        ]);
+    }
+}
